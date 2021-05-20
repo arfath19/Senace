fa85a98c00b1586933a93dcc09b795fd63be6efa
Arfath Ahmed <arfathahmed19@gmail.com>
2021-05-20 19:16:50 +0530

    Ui done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Senace';
+  pushValue;
+  array = [];
+  poppedValues = [];
+  push(val) {
+    if (val.toString().trim() !=="") {
+      this.array.push(val);
+      document.querySelector('.stack').setAttribute('style', `grid-template-rows: repeat(${this.array.length},1fr)`);
+      document.querySelector('.list').setAttribute('style', `grid-template-columns: repeat(${this.poppedValues.length},1fr)`);
+      document.querySelector('.arrayValues').setAttribute('style', `grid-template-columns: repeat(${this.array.length},1fr)`);
+    }
+  }
+
+  pop() {
+    if (this.array.length > 0) {
+      this.poppedValues.push(this.array.pop());
+      document.querySelector('.stack').setAttribute('style', `grid-template-rows: repeat(${this.array.length},1fr)`);
+      document.querySelector('.list').setAttribute('style', `grid-template-columns: repeat(${this.poppedValues.length},1fr)`);
+      document.querySelector('.arrayValues').setAttribute('style', `grid-template-columns: repeat(${this.array.length},1fr)`);
+    }
+  }
+
+  shift() {
+    if (this.array.length > 0) {
+      this.poppedValues.push(this.array.shift());
+      document.querySelector('.stack').setAttribute('style', `grid-template-rows: repeat(${this.array.length},1fr)`);
+      document.querySelector('.list').setAttribute('style', `grid-template-columns: repeat(${this.poppedValues.length},1fr)`);
+      document.querySelector('.arrayValues').setAttribute('style', `grid-template-columns: repeat(${this.array.length},1fr)`);
+    }
+  }
 }
